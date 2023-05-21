832b624b698eb0ea33350a6e8240cf61becaf75d
ғᴧʟʟᴇɴ-✗ <thefunkyfox2004@gmail.com>
2023-05-21 19:30:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     API_ID = "8289355"
     API_HASH = "55822f9d50c5b011177539545f760852"
     TOKEN = "6070111273:AAG31aLGSw4N_KQh6MccxibLjBV-0PxeN0w"  
-    START_PIC = "" 
+    START_PIC = "https://telegra.ph/file/b527f0681f34abbe53e39.jpg" 
     CHAT = "FallenSupport"    
