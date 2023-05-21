45edfba4bbe857d3c1e1a2c239a11704465925bb
ғᴧʟʟᴇɴ-✗ <thefunkyfox2004@gmail.com>
2023-05-21 18:41:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
+    API_ID = "8289355"
+    API_HASH = "55822f9d50c5b011177539545f760852"
+    TOKEN = "6070111273:AAG31aLGSw4N_KQh6MccxibLjBV-0PxeN0w"  
     START_PIC = "" 
-    CHAT = ""    
+    CHAT = "FallenSupport"    
